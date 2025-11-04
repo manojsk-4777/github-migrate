6d110478710e65a1e739e32fa7c922f85d982d07
manojsk-4777 <manoj.sk@applogiq.org>
2025-11-04 14:27:30 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Welcome to My Simple React App 🚀</h1>
+      <p>This is a basic React setup running successfully!</p>
+    </div>
+  );
+}
+
+export default App;
